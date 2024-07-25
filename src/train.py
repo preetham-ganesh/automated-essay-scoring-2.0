3e106cc5ab5d9f5f1b924c05f73fe2b191c50a5f
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2024-07-25 12:42:14 -0400

    [#7] added load_model_configuration() to Train class.
    Loads the model configuration file for current version.

--- src/train.py
+++ src/train.py
@@ -1,3 +1,8 @@
+import os
+
+from src.utils import load_json_file
+
+
 class Train(object):
     """Trains the automatic essay scoring model based on the configuration."""
 
@@ -18,3 +23,22 @@ class Train(object):
         # Initalizes class variables.
         self.model_version = model_version
         self.best_validation_loss = None
+
+    def load_model_configuration(self) -> None:
+        """Loads the model configuration file for current version.
+
+        Loads the model configuration file for current version.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+        self.home_directory_path = os.getcwd()
+        model_configuration_directory_path = "{}/configs/models/essay_scorer".format(
+            self.home_directory_path
+        )
+        self.model_configuration = load_json_file(
+            "v{}".format(self.model_version), model_configuration_directory_path
+        )
