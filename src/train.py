aed8abaad91c0d344961962c0a8928a56b6f98e5
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2024-07-25 19:01:28 -0400

    [#7] added save_model() to Train class.
    Saves the model after checking performance metrics in current epoch.

--- src/train.py
+++ src/train.py
@@ -414,3 +414,17 @@ class Train(object):
             },
             step=epoch,
         )
+
+    def save_model(self) -> None:
+        """Saves the model after checking performance metrics in current epoch.
+
+        Saves the model after checking performance metrics in current epoch.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+        self.manager.save()
+        print("Checkpoint saved at {}.".format(self.checkpoint_directory_path))
